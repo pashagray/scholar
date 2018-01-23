60b0d1e09aab1598075477c982c053aa958df430
Timchenko Ilya <skiny66.ilya@gmail.com>
2018-01-23 10:01:56 +0600

    add folder name to settings for aws

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -4,6 +4,6 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "uploads/#{Setting.school_title_short}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Setting.aws_folder_name}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
